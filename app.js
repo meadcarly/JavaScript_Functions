703468a766d4c11345cc2594192c946608a6e900
Carly Mead <meadcarly16@gmail.com>
2024-08-16 12:58:40 -0600

    Created a function that filters a number count by odd numbers and prints to the console

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+function printOdds(count){
+    if(count < 0){
+        count * -1;
+    }
+    for(let i = 0; i <= count; i++){
+        if(i % 2 !== 0){
+            console.log(i);
+        }
+    }
+}
 
+printOdds(30);
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
